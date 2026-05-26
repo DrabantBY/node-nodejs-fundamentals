e731867bd5878adfce7cfc7863c4f027bbf632aa
DrabantBY <yauheni.basalai@gmail.com>
2026-05-26 22:09:00 +0300

    feat: implement cli/interactive

--- src/cli/interactive.js
+++ src/cli/interactive.js
@@ -1,8 +1,40 @@
-const interactive = () => {
-  // Write your code here
-  // Use readline module for interactive CLI
-  // Support commands: uptime, cwd, date, exit
-  // Handle Ctrl+C and unknown commands
+import { createInterface } from "node:readline/promises";
+import { stdin, stdout, uptime, cwd } from "node:process";
+
+const interactive = async () => {
+  const rl = createInterface({ input: stdin, output: stdout });
+  rl.on("close", () => console.log("Goodbye!"));
+  rl.on("SIGINT", () => rl.close());
+
+  while (true) {
+    const line = await rl.question("> ");
+
+    switch (line.trim()) {
+      case "uptime": {
+        console.log(`Uptime: ${uptime().toFixed(2)}s`);
+        break;
+      }
+
+      case "cwd": {
+        console.log(cwd());
+        break;
+      }
+
+      case "date": {
+        console.log(new Date().toISOString());
+        break;
+      }
+
+      case "exit": {
+        rl.close();
+        return;
+      }
+
+      default: {
+        console.log("Unknown command");
+      }
+    }
+  }
 };
 
 interactive();
