80da649c79f6ba4d1f07b06d4f6871b061b3d54f
DrabantBY <yauheni.basalai@gmail.com>
2026-05-28 01:05:48 +0300

    feat: implement hash/verify

--- src/hash/verify.js
+++ src/hash/verify.js
@@ -1,8 +1,40 @@
+import { createHash } from "node:crypto";
+import { createReadStream } from "node:fs";
+import { resolve } from "node:path";
+
+const getPath = (fileName) => resolve(import.meta.dirname, fileName);
+
 const verify = async () => {
-  // Write your code here
-  // Read checksums.json
-  // Calculate SHA256 hash using Streams API
-  // Print result: filename — OK/FAIL
+  try {
+    let json = "";
+
+    for await (const chunk of createReadStream(
+      getPath("checksums.json"),
+      "utf-8",
+    )) {
+      json += chunk;
+    }
+
+    const checksums = JSON.parse(json);
+
+    for (const fileName of Object.keys(checksums)) {
+      const hash = createHash("sha256");
+
+      for await (const chunk of createReadStream(getPath(fileName))) {
+        hash.update(chunk);
+      }
+
+      console.log(
+        `${fileName} — ${hash.digest("hex") === checksums[fileName] ? "OK" : "FAIL"}`,
+      );
+    }
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    } else {
+      console.log(`Error: ${error.message}`);
+    }
+  }
 };
 
 await verify();
