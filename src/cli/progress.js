ecd272704a0a36320ed23319e37c584a6f399c1e
DrabantBY <yauheni.basalai@gmail.com>
2026-05-27 23:45:15 +0300

    feat: implement cli/progress

--- src/cli/progress.js
+++ src/cli/progress.js
@@ -1,8 +1,69 @@
+import { parseArgs } from "node:util";
+import { stdout } from "node:process";
+
+const getParams = () => {
+  const { values } = parseArgs({
+    options: {
+      duration: {
+        type: "string",
+        default: "5000",
+      },
+      interval: {
+        type: "string",
+        default: "100",
+      },
+      length: {
+        type: "string",
+        default: "30",
+      },
+      color: {
+        type: "string",
+      },
+    },
+  });
+
+  return {
+    duration: Number(values.duration),
+    interval: Number(values.interval),
+    length: Number(values.length),
+    color: values.color,
+  };
+};
+
+const getColor = (color) => {
+  if (!color || !/^#[a-f\d]{6}$/i.test(color)) return "";
+
+  const rgb = color
+    .match(/[a-f\d]{2}/gi)
+    .map((part) => parseInt(part, 16))
+    .join(";");
+
+  return `\x1b[38;2;${rgb}m`;
+};
+
 const progress = () => {
-  // Write your code here
-  // Simulate progress bar from 0% to 100% over ~5 seconds
-  // Update in place using \r every 100ms
-  // Format: [████████████████████          ] 67%
+  const { duration, interval, length, color } = getParams();
+
+  let counter = duration;
+
+  const rgb = getColor(color);
+
+  stdout.write(`\r[${" ".repeat(length)}] 0%`);
+
+  const timer = setInterval(() => {
+    counter -= interval;
+
+    const rate = Math.min((duration - counter) / duration, 1);
+    const size = Math.round(rate * length);
+    const body = `${rgb}${"█".repeat(size)}\x1b[0m${" ".repeat(length - size)}`;
+
+    stdout.write(`\r[${body}] ${Math.round(rate * 100)}%`);
+
+    if (counter < 0) {
+      clearInterval(timer);
+      console.log("\nDone!");
+    }
+  }, interval);
 };
 
 progress();
